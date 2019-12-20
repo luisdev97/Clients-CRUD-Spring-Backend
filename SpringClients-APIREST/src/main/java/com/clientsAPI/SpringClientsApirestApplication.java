0cffbcfccf0ed6953c40ab0b590974b65bb8dd7d
luisdev97 <amadormontorodavid@gmail.com>
2019-12-20 23:13:28 +0100

    Client Entity created

---
+++ SpringClients-APIREST/src/main/java/com/clientsAPI/SpringClientsApirestApplication.java
@@ -0,0 +1,13 @@
+package com.clientsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientsApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClientsApirestApplication.class, args);
+	}
+
+}
---
+++ SpringClients-APIREST/src/main/java/com/clientsAPI/models/entity/Client.java
@@ -0,0 +1,79 @@
+package com.clientsAPI.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+	
+
+ //Cuando la clase se llama igual que la tabla de la base de datos no es necesaria la notacion @Table
+ @Entity
+ @Table(name="clients")
+ public class Client implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	//Cuando el nombre del atributo se llama igual al nombre del campo se puede omitir el @Column()
+	
+	private String name;
+	private String surname;
+	private String email;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
